3e88fb8126caf573ff04e1f18ac174f8f5fc5535
Dale Emery <dale@dhemery.com>
2011-11-22 17:10:17 -0800

    Refactoring regex and scanner

--- src/word_counter.rb
+++ src/word_counter.rb
@@ -1,9 +1,9 @@
 class WordCounter
 
     def self.count_words_on card
-        counts = {}
-        return counts if card == nil
-        card.upcase.scan(/[a-zA-Z]+/).group_by {|x| x}.each{|word, count| counts[word] = count.count}
-        counts
+        re = /([[:alpha:]]+)[ \.]*/
+        return {} if card == nil
+        # card.upcase.scan(/[a-zA-Z]+/).group_by {|x| x}.each{|word, instances| counts[word] = instances.count}
+        Hash[card.upcase.scan(re).flatten.group_by {|word| word}.map {|word, instances| [word, instances.count] }]
     end
 end
